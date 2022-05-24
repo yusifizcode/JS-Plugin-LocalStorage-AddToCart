e35f7bc9c144375ea159dc5ae433c73fed25543f
yusifizcode <5ba0gh2@code.edu.az>
2022-05-25 02:14:08 +0400

    js local storage: basket task complated

--- js/cart.js
+++ js/cart.js
@@ -27,4 +27,46 @@ else{
     let tdCount = document.createElement('td');
     tdCount.innerText = item.Count;
     tr.append(tdCount);
+
+    let tdClose = document.createElement('td');
+    let anchor = document.createElement('a');
+    tdClose.append(anchor);
+    
+    anchor.setAttribute('href','');
+    anchor.className = 'btn btn-outline-danger';
+    anchor.style.textDecoration = 'none';
+    anchor.innerText = 'X';
+    anchor.style.color = 'white';
+    anchor.style.fontSize = 15 + 'px';
+    anchor.style.fontWeight = 700;
+    anchor.style.cursor = 'pointer';
+    tdClose.style.paddingLeft = 15 + 'px';
+    tr.append(tdClose);
+
+
+
+    tdClose.addEventListener('click',function(){
+        console.log("nothing: "+cartItems);
+        // [...cartItems].pop(this.parentNode);
+
+
+        let removedEl = [...cartItems].find(x=>x.Id==tdClose.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML);
+        console.log(removedEl);
+        // [...cartItems][0] = removedEl;
+        // console.log("0:"+[...cartItems][0].Id);
+        // [...cartItems].shift();
+        let remove = [...cartItems].indexOf(removedEl);
+        console.log("removed: "+remove);
+        cartItems.splice(Number(remove),1);
+        // console.log("pop: "+cartItems);
+        console.log("spliced: "+cartItems);
+
+        localStorage.setItem('cart',JSON.stringify(cartItems));
+        
+        // console.log(tdClose.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML);
+        // console.log(cartItems[0]);
+        // [...cartItems].forEach(x=>console.log(x));
+    })
+
 })
+console.log(cartItems);
